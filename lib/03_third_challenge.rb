3482992cc53baba9a5ac2b8a582a0c955cb6d332
Winston Chen <winstonchen.wm@gmail.com>
2020-10-28 20:32:30 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,21 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        name: "lord Montague", age: "53"
-        },
-      :matriarch => {
-        name: "Lady Montague", age: "54"
-        },
-      :hero => {
-        name: "Romeo", age: "15", status: "alive"
-        },
+      :patriarch => {name: "lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: alive"},
+      :heroine => {name: "Juliet", age: "15", status: alive"}, 
       :heroine_friends => []
    }
   }
