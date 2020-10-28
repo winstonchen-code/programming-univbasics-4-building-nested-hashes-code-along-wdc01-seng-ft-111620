4e5969771ad04b5aaf8c855a56b2ebbb81d029ad
Winston Chen <winstonchen.wm@gmail.com>
2020-10-28 20:30:27 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -13,8 +13,7 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"
-        },
+      :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {
         name: "Lady Capulet", age: "51"
         },
